0836dd80d72464d145d500c9bc7dc843a76c61eb
Chang Sau Sheong <sausheong@gmail.com>
2015-09-19 00:16:52 +0800

    added license header, some comments

--- main.go
+++ main.go
@@ -1,3 +1,23 @@
+// Copyright (c) 2015 Chang Sau Sheong
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 package main
 
 import (
@@ -71,7 +91,7 @@ func allReadings(w http.ResponseWriter, r *http.Request) {
 	w.Write(jsonData)
 }
 
-// get PSI data for a specific region: rNO, rSO, rEA, rWE, rCE
+// get PSI data for a specific region: NRS, rNO, rSO, rEA, rWE, rCE
 func region(w http.ResponseWriter, r *http.Request) {
 	region := r.FormValue("r")
 	data, err := grabData()
@@ -83,6 +103,7 @@ func region(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(strconv.Itoa(reading(region, data))))
 }
 
+// get PSI readings and descriptors for all regions, in JSON
 func allRegions(w http.ResponseWriter, r *http.Request) {
 	data, err := grabData()
 	if err != nil {
@@ -134,6 +155,7 @@ func reading(region string, data Data) int {
 	return 0
 }
 
+// description of PSI level
 func describe(psi int) string {
 	switch {
 	case psi <= 50:
